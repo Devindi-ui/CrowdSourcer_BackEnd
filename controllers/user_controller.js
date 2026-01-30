223acf5640d9247cd9ccfadc207ce239a7fa4545
Devindi-u <panchalidevindi@gmail.com>
2026-01-30 19:37:36 +0530

    update code

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -4,20 +4,32 @@ const user = require('../models/user_model');
 // create user
 const users = {
     createUser: async(req,res) => {
+        console.log("req body> ", req.body);
+        
         try {
             const {name, email, password, phone, role_name} = req.body;
+
+            if (!role_name) {
+                return res.status(400).json({message: "Role is required"});
+            }
 
             //get role_id by role_name
             const role_id = await role.getRoleIdByName(role_name);
 
             //save user with role_id
-            const [result] = await user.save({name,email,password,phone,role_id,status_d:1});
+            const result = await user.save({
+                name,
+                email,
+                password,
+                phone,
+                role_id,
+                status_d:1
+            });
 
-            if (result.affectedRows === 1){
+            if (result[0].affectedRows === 1){
                 res.status(201).json({
                     success: true,
                     msg:`User created successfully!!`, 
-                    data:req.body
                 });       
             } else {
                 res.status(400).json({
@@ -28,29 +40,29 @@ const users = {
 
         } catch (error) {
             console.error("User creating error: ", error);
-            
-            res.status(500).json({error: error.message});
+            res.status(500).json({message: "Internal Server Error",error: error.message});
         }
     },
 
     getAllUsers: async(req,res) => {
         try {
-            const [result] = await user.findAll();
+            const result = await user.findAll();
+
             if(result.length === 0){
                 res.status(404).json({msg: 'Users not found'});
                 return;
             }
             res.status(200).json({data: result});
         } catch (error) {
-            res.status(500).json({message: 'Server Error', error});
+            res.status(500).json({message: 'Server Error', error: error.message});
         }
     },
 
     getUserById: async(req,res) => {
         try {
-            const [result] = await user.findById(req.params.id);
-            if(result.length === 0){
-                return res.status(200).json({msg: 'User not found'});
+            const result = await user.findById(req.params.id);
+            if(!result){
+                return res.status(404).json({msg: 'User not found'});
             }
             res.status(200).json({data: result});
         } catch (error) {
@@ -60,7 +72,7 @@ const users = {
 
     getUserByText: async(req,res) => {
         try {
-            const [result] = await user.findByText(req.params.text);
+            const result = await user.findByText(req.params.text);
             if(result.length === 0){
                 return res.status(404).json({msg: "User not found"});
             }
@@ -72,10 +84,24 @@ const users = {
 
     updateUser: async(req,res) => {
         try {
-            const {name, email, password, phone, role_id} = req.body;
+            const {name, email, password, phone, role_name} = req.body;
             const id = req.params.id;
-            const [result] = await user.update({name, email, password, phone, role_id, id});
-            if(result.affectedRows === 0){
+
+            //convert role_name > role_id 
+            const role_id = role_name
+                ? await role.getRoleIdByName(role_name)
+                : null;
+
+            const result = await user.update({
+                id,
+                name, 
+                email, 
+                password, 
+                phone, 
+                role_id
+            });
+
+            if(result[0].affectedRows === 0){
                 return res.status(404).json({message: "User not found"})
             }
             res.status(200).json({message: "User updated successfully!!"});
@@ -86,10 +112,12 @@ const users = {
 
     deleteUser: async(req,res) => {
         try {
-            const [result] = await user.delete(req.params.id);
-            if(result.affectedRows === 0){
+            const result = await user.delete(req.params.id);
+
+            if(result[0].affectedRows === 0){
                 return res.status(404).json({msg: "User not found"});
             }
+
             res.status(200).json({message: "User deleted successfully!"});
         } catch (error) {
             res.status(500).json({message: 'Internal Server Error', error: error.message});
--- models/user_model.js
+++ models/user_model.js
@@ -1,42 +1,80 @@
 const db = require('../db');
-const { findById } = require('./role_model');
 
 const user = {
     save: (user) => {
         const {name, email, password, phone, role_id, status_d} = user;
-        const sql = "INSERT INTO user (name, email, password, phone, role_id, status_d) VALUES (?,?,?,?,?,?)";
+        const sql = `INSERT INTO user (name, email, password, phone, role_id, 
+                     status_d) VALUES (?,?,?,?,?,?)`;
         return db.execute(sql, [name,email,password,phone,role_id, status_d]);
     },
 
     findAll: async() => {
-        const sql = `SELECT * FROM user WHERE user.status_d=1
-                    ORDER BY name DESC`;
-        return db.execute(sql);
+        const [rows] = await db.query (`
+                    SELECT 
+                        u.user_id,
+                        u.name,
+                        u.email,
+                        u.phone,
+                        r.role_name 
+                    FROM user u
+                    LEFT JOIN role r ON u.role_id = r.role_id
+                    WHERE u.status_d = 1 
+                    ORDER BY u.name DESC
+        `);
+        return rows;
     },
 
     findById: async(id) => {
-        const sql = `SELECT * FROM user WHERE user_id=?
-                    AND user.status_d=1`;
-        return db.execute(sql,[id]);
+        const [rows] = await db.query (`
+            SELECT 
+                u.user_id,
+                u.name,
+                u.email,
+                u.phone,
+                r.role_name
+            FROM user u 
+            LEFT JOIN role r ON u.role_id = r.role_id 
+            WHERE u.user_id = ? 
+            AND u.status_d=1
+        `, [id]);
+        return rows[0];
     },
 
     findByText: async(input) => {
-        const searchText = `%${input}%`
-        const sql = `SELECT * FROM user WHERE name LIKE ? OR email LIKE ? 
-                    OR password LIKE ? OR phone LIKE ? OR role_id LIKE ? 
-                    AND user.status_d=1`;
-        return db.execute(sql, [searchText, searchText, searchText, 
-                          searchText, searchText]);
+        const searchText = `%${input}%`;
+        const [rows] = await db.query (`
+                    SELECT 
+                      u.user_id,
+                      u.name,
+                      u.email,
+                      u.phone,
+                      r.role_name
+                    FROM user u
+                    LEFT JOIN role r ON u.role_id = r.role_id 
+                    WHERE (
+                      u.name LIKE ? 
+                      OR u.email LIKE ? 
+                      OR u.phone LIKE ? 
+                      OR r.role_name LIKE ?
+                    )
+                    AND u.status_d = 1
+        `,
+        [searchText, searchText, searchText, searchText]);
+        return rows;
     },
 
     update: (user) => {
         const {id, name, email, password, phone, role_id} = user;
-        const sql = "UPDATE user SET name=?, email=?, password=?, phone=?, role_id=? WHERE user_id=?";
+        const sql = `
+            UPDATE user 
+            SET name=?, email=?, password=?, phone=?, role_id=? 
+            WHERE user_id = ?
+        `;
         return db.execute(sql, [name, email, password, phone, role_id, id]);
     },
 
     delete: (id) => {
-        const sql = "UPDATE user SET user.status_d=0 WHERE user_id=?";
+        const sql = "UPDATE user SET status_d = 0 WHERE user_id = ?";
         return db.execute(sql, [id]);
     }
 
